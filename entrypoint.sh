5ad9d1f79461fe0451380c48a8591a6a5ee79607
Jiří Kulík <jiri.kulik@jkulik.eu>
2018-11-18 20:48:28 +0100

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,12 +1,12 @@
 #!/bin/sh
 
-RUN mkdir -p /root/.ssh && \
-    chmod 0700 /root/.ssh && \
-    echo "$ssh_prv_key" > /root/.ssh/jiri_kulik && \
-    echo "$ssh_pub_key" > /root/.ssh/jiri_kulik.pub && \
-    chmod 600 /root/.ssh/id_rsa && \
-    chmod 600 /root/.ssh/id_rsa.pub && \
-    ssh-add ~/.ssh/jiri_kulik
+mkdir -p /root/.ssh
+chmod 0700 /root/.ssh
+echo "$ssh_prv_key" > /root/.ssh/jiri_kulik
+echo "$ssh_pub_key" > /root/.ssh/jiri_kulik.pub
+chmod 600 /root/.ssh/id_rsa
+chmod 600 /root/.ssh/id_rsa.pub
+ssh-add ~/.ssh/jiri_kulik
 
 if [ -n "$1" ]
 then
