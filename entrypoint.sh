44a2d782f0ed132b7f622ee2b84d29da2016c078
Jiří Kulík <jiri.kulik@jkulik.eu>
2018-11-19 08:34:57 +0100

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -6,8 +6,8 @@ echo "$ssh_prv_key" > /root/.ssh/jiri_kulik
 echo "$ssh_pub_key" > /root/.ssh/jiri_kulik.pub
 chmod 600 /root/.ssh/jiri_kulik
 chmod 600 /root/.ssh/jiri_kulik.pub
-#eval `ssh-agent -s`
-#ssh-add ~/.ssh/jiri_kulik
+eval $(ssh-agent -s)
+ssh-add ~/.ssh/jiri_kulik
 
 if [ -n "$1" ]
 then
