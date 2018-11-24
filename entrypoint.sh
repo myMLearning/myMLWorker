3306108d85c6bcf5e2e3a12fd061d25d7d011180
Jiří Kulík <jiri.kulik@jkulik.eu>
2018-11-24 14:59:31 +0100

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,14 +1,21 @@
 #!/bin/sh
 
+#Setup ssh folder
 mkdir -p /root/.ssh
 chmod 0700 /root/.ssh
+
+#Add ssh keys from command line arguments (used for bitbucket)
 echo "$ssh_prv_key" > /root/.ssh/jiri_kulik
 echo "$ssh_pub_key" > /root/.ssh/jiri_kulik.pub
 chmod 600 /root/.ssh/jiri_kulik
 chmod 600 /root/.ssh/jiri_kulik.pub
+
+#Register the ssh keys
 eval $(ssh-agent -s)
 ssh-add ~/.ssh/jiri_kulik
 
+#Run a command line argument to the entrypoint.sh
+#Used to run nomachine script
 if [ -n "$1" ]
 then
     exec "$1"
